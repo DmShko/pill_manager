1250cf49bc5cba96300f4412f8009f6572c3960d
DmShko <vombat_schev@i.ua>
2024-06-10 21:35:39 +0300

    pill_manager-69

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),], 
-  base: "/",
+  base: "https://medicine-manager.netlify.app/",
 })
 // base: "/medicine/",
